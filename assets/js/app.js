0ea6c1d963f9a90f09c41a85b6358f01bea7d061
Andrew Yan <andrew@Andrews-MacBook-Pro.local>
2019-06-06 12:53:35 -0400

    First Commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const signature = document.querySelectorAll("#signature path");
+c
+
+for(let i = 0; i<signature.length; i++){
+    console.log('Letter ${i} is ${signature[i].getTotalLength()}');
+}
+
+
