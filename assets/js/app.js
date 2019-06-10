903a54c823f98bc640252b81c56eccc7341efa09
Andrew Yan <andrew@Andrews-MBP.fios-router.home>
2019-06-09 21:06:26 -0400

    Add existing file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,2 @@
 const signature = document.querySelectorAll("#signature path");
-c
-
-for(let i = 0; i<signature.length; i++){
-    console.log('Letter ${i} is ${signature[i].getTotalLength()}');
-}
-
 
